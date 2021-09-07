36d2ca95788787ffd7fd39baa51c381384b990a4
Serena <serenaqkong@gmail.com>
2021-09-07 12:46:01 -0700

    import interface for props, add Container component

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,24 @@
-function Home(){
-  return <div>stuff</div>
+import Container from "./Container"
+import {User, Message} from "./interfaces";
+
+interface UserMessage {
+  users: User[];
+  messages: Message[];
+}
+
+function Home(props: UserMessage){
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <Container users={props.users} />
+        </div>
+        <div className="col-6">
+          <Container messages={props.messages} />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home;
