9b317aad06acc331a9a7a487c17ae4a8b85e45ed
Serena <serenaqkong@gmail.com>
2021-09-07 13:25:53 -0700

    added bootstrap card formatting

--- src/MessageCard.tsx
+++ src/MessageCard.tsx
@@ -1,16 +1,18 @@
 import {Message} from "./interfaces";
 
 interface MessageProps {
-    message: Message;
+  message: Message;
 }
 
 function MessageCard(props: MessageProps): React.ReactElement {
-    return (
-        <div>
-            <p>Message: {props.message.body}</p>
-            <p>User: {props.message.user.username}</p>
+  return (
+    <div className="card mb-2 bg-light">
+      <div className="card-body">
+        <p className="card-text">Message: {props.message.body}</p>
+        <p className="card-text">User: {props.message.user.username}</p>
         </div>
-    )
+    </div>
+  )
   }
   
   export default MessageCard;
--- src/UserCard.tsx
+++ src/UserCard.tsx
@@ -1,17 +1,19 @@
 import {User} from "./interfaces";
 
 interface UserProps {
-    user: User;
+  user: User;
 }
 
 function UserCard(props: UserProps): React.ReactElement {
-    return (
-        <div>
-            <p>Username: {props.user.username}</p>
-            <p>First Name: {props.user.firstName}</p>
-            <p>Last Name: {props.user.lastName}</p>
-        </div>
-    )
+  return (
+    <div className="card mb-2 bg-light">
+      <div className="card-body">
+        <p className="card-text">Username: {props.user.username}</p>
+        <p className="card-text">First Name: {props.user.first_name}</p>
+        <p className="card-text">Last Name: {props.user.last_name}</p>
+      </div>
+    </div>
+  )
   }
   
   export default UserCard;
