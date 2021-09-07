0b8d58b7f49cb110a91b5e5680f4c491296b39c9
Serena <serenaqkong@gmail.com>
2021-09-07 13:24:25 -0700

    added function and useEffect for getting allUserMessages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import "bootstrap/dist/css/bootstrap.css"
 import { BrowserRouter } from "react-router-dom";
 import UserMessagesApi from "./api";
@@ -6,11 +6,39 @@ import Routes from "./Routes";
 import Nav from "./Nav";
 
 function App(): React.ReactElement {
+  const [users, setUsers] = useState([]);
+  const [messages, setMessages] = useState([]);
+  const [user, setUser] = useState({});
+  const [message, setMessage] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function createUser() {
+  
+  }
+
+  async function createMessage() {}
+
+  async function getAllUserMessages() {
+    setIsLoading(true);
+    let resultFromAPI = await UserMessagesApi.getAllUserMessages();
+    setUsers(resultFromAPI.users);
+    setMessages(resultFromAPI.messages);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    getAllUserMessages();
+  }, []);
+
+  if (isLoading) {
+    return <h1>...Loading...</h1>
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
         <Nav />
-        <Routes />
+        <Routes users={users} messages={messages}/>
       </BrowserRouter>
     </div>
   );
