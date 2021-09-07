bd744867a11517d40efe4b0e2e129da8b4d82ccf
Serena <serenaqkong@gmail.com>
2021-09-07 15:43:31 -0700

    added docstring

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,19 +1,26 @@
 import UserCard from "./UserCard";
 import MessageCard from "./MessageCard";
 import {User, Message} from "./interfaces";
+import {Link} from "react-router-dom"
 
 interface UserMessage {
   users: User[];
   messages: Message[];
 }
-
+/** Home
+ * 
+ */
 function Home(props: UserMessage): React.ReactElement{
   return (
     <div className="container mt-2">
       <div className="row">
         <div className="col-6">
           {props.users.map(u => (
-            <UserCard user={u} key={u.username}/>
+            <Link 
+              key={u.username}
+              to={`/users/${u.username}`}>
+                <UserCard user={u} key={u.username}/>
+            </Link>
           ))}
         </div>
         <div className="col-6">
